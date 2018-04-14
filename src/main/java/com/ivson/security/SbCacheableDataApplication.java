da2dfb9350bc06f1887ba725eb5b305108e6b051
Ivson Santos <santosivson@hotmail.com>
2018-04-14 19:43:33 -0300

    Adicionando o cacheamento, muito mais rapido

--- src/main/java/com/ivson/security/SbCacheableDataApplication.java
+++ src/main/java/com/ivson/security/SbCacheableDataApplication.java
@@ -2,8 +2,10 @@ package com.ivson.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SbCacheableDataApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ivson/security/repository/SimpleBookRepository.java
+++ src/main/java/com/ivson/security/repository/SimpleBookRepository.java
@@ -1,5 +1,6 @@
 package com.ivson.security.repository;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import com.ivson.security.model.Book;
@@ -8,6 +9,7 @@ import com.ivson.security.model.Book;
 public class SimpleBookRepository implements BookRepository {
 
 	@Override
+	@Cacheable("books")
     public Book getByIsbn(String isbn) {
         simulateSlowService();
         return new Book(isbn, "Some book");
